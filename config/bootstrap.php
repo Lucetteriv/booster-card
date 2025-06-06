ad1815edce894759122d673aeedad8732138c2e0
lucetteriv <com.lucie.ri@gmail.com>
2025-06-06 16:32:24 +0200

    init project

---
+++ config/bootstrap.php
@@ -0,0 +1,10 @@
+ <?php
+
+ use Dotenv\Dotenv;
+
+ require_once '../vendor/autoload.php';
+
+ $dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+ $dotenv->load();
+
+ require_once __DIR__ . '/database.php';
